723064e6a203a761a3485d3c75fb5b579a2ab174
kind2war <130664778+kind2war@users.noreply.github.com>
2023-08-21 08:06:28 +0000

    [fix]デフォルト値設定の削除

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 2023_08_16_014449) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "change_requests", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "parks_id"
+    t.text "order_detail", null: false
+    t.integer "request_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_change_requests_on_member_id"
+    t.index ["parks_id"], name: "index_change_requests_on_parks_id"
+  end
+
   create_table "children", force: :cascade do |t|
     t.integer "member_id"
     t.integer "sex", default: 0, null: false
@@ -137,8 +148,22 @@ ActiveRecord::Schema.define(version: 2023_08_16_014449) do
     t.index ["park_id"], name: "index_review_comments_on_park_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "park_id"
+    t.string "title", default: "", null: false
+    t.text "review_detail", null: false
+    t.boolean "draft_flag", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_reviews_on_member_id"
+    t.index ["park_id"], name: "index_reviews_on_park_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "change_requests", "members"
+  add_foreign_key "change_requests", "parks", column: "parks_id"
   add_foreign_key "children", "members"
   add_foreign_key "features", "parks"
   add_foreign_key "park_feature_relations", "features"
@@ -147,4 +172,6 @@ ActiveRecord::Schema.define(version: 2023_08_16_014449) do
   add_foreign_key "parks", "reviews"
   add_foreign_key "review_comments", "members"
   add_foreign_key "review_comments", "parks"
+  add_foreign_key "reviews", "members"
+  add_foreign_key "reviews", "parks"
 end
