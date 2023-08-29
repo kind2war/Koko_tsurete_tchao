c9819c48b4220d559be6b9d4af9d5761a90093c5
kind2war <130664778+kind2war@users.noreply.github.com>
2023-08-29 06:14:02 +0000

    [fix]method::delete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require jquery
+//= require jquery_ujs
 import Rails from "@rails/ujs"
 import $ from "jquery"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-//raty
-import Raty from "raty.js"
-window.raty = function(elem,opt) {
-  let raty =  new Raty(elem,opt)
-  raty.init();
-  return raty;
-}
 
 Rails.start()
 Turbolinks.start()
