df26e5ea78aafb86175fa3d45b78bd35142fc3d8
kind2war <130664778+kind2war@users.noreply.github.com>
2023-09-07 14:08:45 +0000

    [fix]flashの対処

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -5,8 +5,11 @@ before_action :authenticate_member!
     park = Park.find(params[:park_id])
     review = current_member.reviews.new(review_params)
     review.park_id = park.id
-    review.save
-    redirect_to park_path(park)
+    if review.save
+      redirect_to park_path(park), notice: "投稿しました"
+    else
+      render "new"
+    end
   end
 
   def destroy
