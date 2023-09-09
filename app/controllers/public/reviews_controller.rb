11676490f607777afe4d9d973aa420def891a542
kind2war <130664778+kind2war@users.noreply.github.com>
2023-09-09 23:59:59 +0000

    [fix]review_messeage

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -5,21 +5,30 @@ before_action :authenticate_member!
     park = Park.find(params[:park_id])
     review = current_member.reviews.new(review_params)
     review.park_id = park.id
-    if review.save
+    if park.valid? && review.valid?
+      # 両方のモデルがバリデーションをパスした場合の処理
+      park.save
+      review.save
       redirect_to park_path(park), notice: "投稿しました"
     else
-      render "new"
+      # バリデーションエラーがある場合の処理
+      flash[:alert] = "入力に不備があります"
+      @park = Park.find(params[:park_id])
+      @review = current_member.reviews.new(review_params)
+      review.park_id = park.id
+      render template: "/public/parks/show"
     end
   end
 
   def destroy
     Review.find(params[:id]).destroy
-    redirect_to park_path(params[:park_id])
+    redirect_to park_path(params[:park_id]), notice: "レビューを削除しました"
   end
 
  private
   def review_params
     params.require(:review).permit(:title, :review_detail, :park_id)
   end
+
 
 end
